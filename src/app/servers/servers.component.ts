106e9fdfcbff3574e519db1874e299c531639f00
Draflo <flo2533@hotmail.fr>
2023-12-12 15:48:16 +0100

    feat: ngmodel, ngclass, ngstyle, dynamic databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrl: './servers.component.css'
+  styleUrl: './servers.component.css',
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created !';
+  serverName = 'Test server';
+  serverCreated = false;
+  servers = ['Server 1', 'Server 2'];
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName)
+    this.serverCreationStatus =
+      'Server was created ! Name is ' + this.serverName;
+  }
 }
