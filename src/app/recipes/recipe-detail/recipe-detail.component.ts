b0342ff5b418eebd298d23e7add4bf0dfb018dbb
Draflo <flo2533@hotmail.fr>
2024-01-30 09:55:01 +0100

    feat: adding create and delete recipe

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,29 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrl: './recipe-detail.component.css'
+  styleUrl: './recipe-detail.component.css',
 })
 export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   name: string;
 
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute) {}
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.name = params['name']
-        this.recipe = this.recipeService.getRecipe(this.name)
-      }
-    )
+    this.route.params.subscribe((params: Params) => {
+      this.name = params['name'];
+      this.recipe = this.recipeService.getRecipe(this.name);
+    });
   }
 
   onToShoppingList() {
-    this.recipeService.addToShoppingList(this.recipe.ingredients)
-    }
+    this.recipeService.addToShoppingList(this.recipe.ingredients);
+  }
+
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.name)
+    this.router.navigate(['/recipes'])
+  }
 }
