72da719941764fcafd4c84f2dab10436963da4e0
Draflo <flo2533@hotmail.fr>
2024-02-12 09:36:32 +0100

    feat: adding guard, interceptor and conditionnal view

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,40 @@
 import { Component } from '@angular/core';
 import { DataSorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-  constructor(private dataStorage: DataSorageService) {}
+  collapsed = true;
+  isAuthenticated = false
+  private userSubscr: Subscription;
+  constructor(
+    private dataStorage: DataSorageService,
+    private authService: AuthService
+  ) {}
 
-onSaveData() {
-  this.dataStorage.saveRecipes()
-}
+  ngOnInit() {
+    this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user
+    });
+  }
 
-onFetchData() {
-  this.dataStorage.fetchRecipes().subscribe()
-}
-  collapsed = true
+  onLogout() {
+    this.authService.logout()
+  }
+
+  onSaveData() {
+    this.dataStorage.saveRecipes();
+  }
+
+  onFetchData() {
+    this.dataStorage.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.userSubscr.unsubscribe();
+  }
 }
